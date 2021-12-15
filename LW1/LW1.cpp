da9c76895f98dd044bc02986e6afba7c34088bee
Bogdan <bogdan.plockiy@mail.ru>
2021-12-15 22:10:13 +0300

    Добавьте файлы проекта.

---
+++ LW1/LW1.cpp
@@ -0,0 +1,62 @@
+﻿#include <vector>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct edge {
+    int to;
+    bool deleted;
+};
+
+const int maxn = 1e5;
+vector<edge> edges;
+stack<int> g[maxn];
+
+void addEdge(int u, int v) {
+    g[u].push(edges.size());
+    edges.push_back({ v, false });
+    g[u].push(edges.size());
+    edges.push_back({ u, false });
+}
+
+void euler(int v) {
+    //cout << !g[v].empty() << endl;
+    while (!g[v].empty()) {
+        int e = g[v].top();
+        g[v].pop();
+        if (!edges[e].deleted) {
+            edges[e].deleted = true;
+            euler(e);
+        }
+    }
+    cout << v << " ";
+}
+
+int main()
+{
+    addEdge(1, 4);
+    addEdge(1, 5);
+    addEdge(1, 2);
+    addEdge(2, 7);
+    addEdge(2, 8);
+    addEdge(3, 5);
+    addEdge(3, 6);
+    addEdge(4, 7);
+    addEdge(4, 8);
+    addEdge(5, 7);
+    addEdge(5, 8);
+    addEdge(6, 7);
+    addEdge(6, 8);
+    addEdge(7, 1);
+    addEdge(7, 9);
+    addEdge(8, 3);
+    addEdge(8, 9);
+    addEdge(9, 4);
+    addEdge(9, 6);
+    //cout << !g[1].empty() << endl;
+
+    euler(1);
+    
+	return 0;
+}
---
+++ LW1_1/LW1_1.cpp
@@ -0,0 +1,160 @@
+﻿#include <vector>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+vector<pair<int, int>> edges;
+
+vector<int> visited_nodes;
+vector<int> answer;
+
+void printEdges()
+{
+	cout << "\tedges:\n";
+	for (int i = 0; i < edges.size(); i++)
+	{
+		cout << " " << i + 1 << ") "
+			<< edges[i].first + 1 << " -> "
+			<< edges[i].second + 1 << endl;
+	}
+	cout << endl;
+}
+
+template<typename  T>
+void printVec(vector<T> v)
+{
+	for (T n : v)
+		cout << ((n >= 0) ? (n + 1) : n) << " ";
+	cout << endl;
+}
+
+void addEdge(int u, int v)
+{
+	edges.push_back(make_pair(u,v));
+}
+
+void readMatr(string str)
+{
+	ifstream fin(str);
+	int sizeMatr, el;
+
+	//считал размер матрицы смежности
+	fin >> sizeMatr;
+	cout << "\tsize: " << sizeMatr << endl;
+
+	//считал саму матрицу смежности
+	for (int i = 0; i < sizeMatr; i++)
+	{
+		for (int j = 0; j < sizeMatr; j++)
+		{
+			fin >> el;
+			if (el)
+			{
+				addEdge(i, j);
+				cout << " added edge: " << i + 1 << " -> " << j + 1 << endl;
+			}
+		}
+	}
+	cout << "count of edges: " << edges.size() << endl;
+}
+
+void euler(int n)
+{
+
+	// выход, если закончились рёбра
+	if (edges.size() == 0)
+	{
+		visited_nodes.push_back(n);
+		return;
+	}
+
+	// проверка на отсутствие ребер из вершины n
+	if (n == -1)
+	{
+		answer.push_back(visited_nodes[visited_nodes.size() - 1]);
+		visited_nodes.pop_back();
+		n = visited_nodes[visited_nodes.size() - 1];
+	}
+	else
+	{
+		// добавление текущей точки в массив посещенных точек
+		visited_nodes.push_back(n);
+	}
+
+	//следующая точка
+	int node = -1;
+
+	// поиск точки, в которую мы можем попасть из текущей точки
+	for (int i = 0; i < edges.size(); i++)
+	{
+		if (edges[i].first == n)
+		{
+			//euler(edges[i].second);
+			node = edges[i].second;
+			edges.erase(edges.begin() + i);
+			break;
+		}
+	}
+
+	// печать вектора посезенных элементов
+	cout << "visited_nodes: ";
+	printVec(visited_nodes);
+	cout << "answer: ";
+	printVec(answer);
+
+	// Печать рёбер
+	printEdges();
+
+	// Расчет следующего пути
+	euler(node);
+	return;
+}
+
+bool isEuler()
+{
+	// Проверка равенства полусумм входа и выхода
+	for (int j = 0; j < edges.size(); j++)
+	{
+		int sumIn = 0, sumOut = 0;
+		for (int i = 0; i < edges.size(); i++)
+		{
+			if (edges[i].first == j)
+				sumIn++;
+			if (edges[i].second == j)
+				sumOut++;
+		}
+		if (sumIn != sumOut)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	// считывание матрицы смежности
+	readMatr("data1.txt");
+
+	// TODO добавить проверку на существование эйлерова цикла
+	if (!isEuler())
+	{
+		cout << "Graph is not euler's\n";
+		return 0;
+	}
+
+	// вызвал метод поиска эйлерова цикла c вершины 0
+	euler(2);
+
+	// Вывод эйлерова цикла
+	reverse(visited_nodes.begin(), visited_nodes.end());
+	answer.insert(answer.end(), visited_nodes.begin(), visited_nodes.end());
+	reverse(answer.begin(), answer.end());
+
+	// печать ответа
+	cout << "answer: ";
+	printVec(answer);
+
+	return 0;
+}
